2f9720b7dae7c3413734036723c9b7552a85b1ff
jesseokeya <jesseokeya@gmail.com>
2019-04-27 18:41:49 -0400

    Update index.js

--- exercises/fizzbuzz/index.js
+++ exercises/fizzbuzz/index.js
@@ -13,7 +13,7 @@
 //   buzz
 
 function fizzBuzz(n) {
-    for (let i = 0; i <= n; i++) {
+    for (let i = 1; i <= n; i++) {
         if (i % 5 === 0 && i % 3 === 0) {
             console.log('fizzbuzz')
         } else if (i % 3 === 0) {
