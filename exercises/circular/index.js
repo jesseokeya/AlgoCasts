662a7474e4a7ace7d72e0650598fb86556115536
jesseokeya <jesseokeya@gmail.com>
2019-07-06 20:16:22 -0400

    Update index.js

--- exercises/circular/index.js
+++ exercises/circular/index.js
@@ -12,6 +12,15 @@
 //   c.next = b;
 //   circular(l) // true
 
-function circular(list) {}
+function circular(list) {
+    let slow, fast
+    slow = fast = list.head
+    while (fast.next && fast.next.next) {
+        slow = slow.next
+        fast = fast.next.next
+        if (slow === fast) { return true }
+    }
+    return false
+}
 
 module.exports = circular;
